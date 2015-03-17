fb15eb90ac19efc693289ee5795c98bb0709293e
jonasbaer <jonas.baer@gmail.com>
2015-03-17 18:02:29 +0700

    second commit

---
+++ WatchKit Positioning WatchKit Extension/PositioningInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  PositioningInterfaceController.swift
+//  WatchKit Positioning
+//
+//  Created by Jonas Baer on 17.03.15.
+//  Copyright (c) 2015 jonas-baer.com. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class PositioningInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
